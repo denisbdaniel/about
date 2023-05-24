a14b0dd1f0daa183b0ba7cc46634fe6da619f506
Dennis <121769718+aesthdennis@users.noreply.github.com>
2023-05-24 11:10:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Slide Animation for Header; don't ask me anything -- I hate this.
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = form.querySelector('input[type="email"]').value;
+  alert(`Thanks for subscribing with email: ${email}`);
+  form.reset();
+});
+
+// Get the menu icon and header links elements
+
+function toggleMenu() {
+  var links = document.querySelector(".links");
+  links.classList.toggle("active");
+}
