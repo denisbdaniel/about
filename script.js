eb57aea0091a94a97ab45fa4303f24f25e0ac338
Dennis <121769718+aesthdennis@users.noreply.github.com>
2023-07-24 21:49:59 +0300

    Add files via upload
    Further

--- script.js
+++ script.js
@@ -23,27 +23,36 @@ function toggleMenu() {
   links.classList.toggle("active");
 }
 
-
 function sendLetter() {
   var name = document.getElementById("name").value;
   var message = document.getElementById("message").value;
 
-    // Encode the subject and body for the mailto link
+  // Encode the subject and body for the mailto link
   var subject = "Letter from " + name;
   var body = message;
 
-   // Construct the mailto link with the encoded subject and body
-  var mailtoLink = "mailto:devotedentity@gmail.com" + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
-  
+  // Construct the mailto link with the encoded subject and body
+  var mailtoLink =
+    "mailto:devotedentity@gmail.com" +
+    "?subject=" +
+    encodeURIComponent(subject) +
+    "&body=" +
+    encodeURIComponent(body);
+
   // Open the default email client with the mailto link
   window.location.href = mailtoLink;
 }
-        function toggleDarkMode() {
-            var body = document.body;
-            body.classList.toggle("dark-mode");
-        }
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
 
+function scrollDown() {
+  // Adjust the number below to control how far the page scrolls down
+  const scrollAmount = 1000; // Change this value based on your needs
 
-
-
-  
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: "smooth",
+  });
+}
